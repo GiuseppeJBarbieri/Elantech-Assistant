958d609c6054b1bba23069c0fa575b226299412e
Giuseppe Barbieri <giuseppe@elantechus.com>
2023-04-02 03:45:45 -0400

    Updated Constants

--- elantech-client/src/constants/API.ts
+++ elantech-client/src/constants/API.ts
@@ -1,4 +1,5 @@
 export const BASE_API_URL = 'http://localhost:3001/api/v1/';
+export const BASE_API_URL_PROD = 'http://69.119.197.202:3001/api/v1/';
 export enum ROUTES  {
     PRODUCTS = 'products',
     COMPANY = 'company',
--- elantech-client/src/constants/Defaults.ts
+++ elantech-client/src/constants/Defaults.ts
@@ -1,6 +1,8 @@
+import IAlert from '../types/IAlert';
 import ICompany from '../types/ICompany';
 import IProduct from '../types/IProduct';
 import IQuote from '../types/IQuote';
+import IQuotedProduct from '../types/IQuotedProduct';
 
 export const defaultProduct = (): IProduct => {
     return JSON.parse(JSON.stringify({
@@ -40,14 +42,26 @@ export const defaultCompany = (): ICompany => {
 export const defaultQuote = (): IQuote => {
     return JSON.parse(JSON.stringify({
         id: 0,
-        companyID: 0,
-        userID: 0,
+        companyId: 0,
+        userId: 0,
         dateQuoted: '',
         sold: false,
     }))
 }
 
-export const defaultAlert = () => {
+export const defaultQuotedProduct = (): IQuotedProduct => {
+    return JSON.parse(JSON.stringify({
+        id: 0,
+        quoteId: 0,
+        productId: 0,
+        quantity: 0,
+        quotedPrice: 0.0,
+        productCondition: '',
+        comment: '',
+    }))
+}
+
+export const defaultAlert = (): IAlert => {
     return JSON.parse(JSON.stringify({
         label: '',
         type: 'danger',
